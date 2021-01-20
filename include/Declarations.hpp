43fe064ec406534b956fe6e66cb011c299b17e75
Tasia11 <61164926+Tasia11@users.noreply.github.com>
2021-01-21 00:46:08 +0300

    Create Declarations.hpp

---
+++ include/Declarations.hpp
@@ -0,0 +1,13 @@
+// Copyright 2020 Your Name <your_email>
+
+#ifndef TEMPLATE_DECLARATIONS_HPP
+#define TEMPLATE_DECLARATIONS_HPP
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/utility/setup.hpp>
+
+const char kEndChar[] = "0000";
+const size_t kEndCharSize = 4;
+void init();
+#endif  // TEMPLATE_DECLARATIONS_HPP
