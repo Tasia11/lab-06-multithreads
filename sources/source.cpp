845617b7e4be93fcc1b716e9fa04f609cbd46a73
Tasia11 <61164926+Tasia11@users.noreply.github.com>
2021-01-21 00:49:13 +0300

    Create source.cpp

---
+++ sources/source.cpp
@@ -0,0 +1,25 @@
+// Copyright 2020 Your Name <your_email>
+
+#ifndef INCLUDE_HEADER_HPP_
+#define INCLUDE_HEADER_HPP_
+#include <ctime>
+#include <fstream>
+#include <iomanip>
+#include <mutex>
+#include <nlohmann/json.hpp>
+#include <sstream>
+#include <string>
+using json = nlohmann::json;
+
+class JSON {
+ public:
+  void addHash(const std::string& data, const std::string& hash,
+               std::time_t timestamp);
+  friend std::ostream& operator<<(std::ostream& out, const JSON& j);
+
+ private:
+  mutable std::mutex mut;
+  json j_array;
+};
+
+#endif // INCLUDE_HEADER_HPP_
