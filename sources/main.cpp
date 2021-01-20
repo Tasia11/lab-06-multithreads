7580edff69d5f19503b955f5463044714b07d0b6
Tasia11 <61164926+Tasia11@users.noreply.github.com>
2021-01-21 00:53:28 +0300

    Update and rename hashCalc.cpp to main.cpp

---
+++ sources/main.cpp
@@ -0,0 +1,20 @@
+// Copyright 2020 Osipova Anastasiya anastasiyaosipova2001@gmail.com
+
+#include "header.hpp"
+
+void JSON::addHash(const std::string& data, const std::string& hash,
+                   std::time_t timestamp) {
+  std::scoped_lock<std::mutex> lock(mut);
+  std::stringstream rightData;
+  rightData << std::hex << std::uppercase << std::stoi(data);
+  json j_map;
+  j_map["data"] = rightData.str();
+  j_map["hash"] = hash;
+  j_map["timestamp"] = timestamp;
+  j_array.push_back(j_map);
+}
+std::ostream& operator<<(std::ostream& out, const JSON& j) {
+  std::scoped_lock<std::mutex> lock(j.mut);
+  out << j.j_array.dump(4);
+  return out;
+}
